790642e8981ce74b6e49b2f4b4f51fc360ac5e44
Mohan-k-03 <mohanarul2003@gmail.com>
2026-08-14 11:49:47 +0530

    making app course navbar and get output

---
+++ React-App/src/Course.jsx
@@ -0,0 +1,12 @@
+const course1="HTML"
+function Course(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <h3>{course1}</h3>
+            <b>This is Mohan's {course1} learning</b>
+            <footer></footer>
+        </div>
+    );
+}
+export default Course;
---
+++ React-App/src/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <nav>
+            <h2> Hello Mohan</h2>
+            <button>login</button>
+
+        </nav>
+    );
+}
+export default Navbar
