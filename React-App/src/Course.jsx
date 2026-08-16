39af8e15df2be9baf43374b49f56ef16b4874f3f
Mohan-k-03 <mohanarul2003@gmail.com>
2026-08-16 12:13:31 +0530

    event click

--- React-App/src/Course.jsx
+++ React-App/src/Course.jsx
@@ -1,12 +1,16 @@
 import css from './assets/css.png'
 function Course(props) {
-  
+  function BuyCourse(discount,e){
+    console.log(props.name,"puchased with", discount,"%discount");
+    console.log(e)
+  }
     return(
             props.name && <div className="card">
                 <img src={props.image} />
                 <h3>{props.name}</h3>
                 <p>{props.price}</p>
                 <span>{props.rating}</span>
+                <button onClick={(event)=>BuyCourse(20,event)} className="buynowbutton">Buy now</button>
             </div>
             );
 }
