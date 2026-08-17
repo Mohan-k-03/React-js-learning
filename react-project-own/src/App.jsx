c0614fc1cdfefd36ace4ac8d448b894d05b88888
Mohan-k-03 <mohanarul2003@gmail.com>
2026-08-17 22:51:21 +0530

    first commit

---
+++ react-project-own/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") {
+      alert("task should not empty");
+      console.log("added");
+      return;
+    }
+    setTasks([...tasks, { text: task, completed: false }]);
+    setTask("");
+  };
+
+  const toggleTask = (index) => {
+    const updatedTasks = tasks.map((t, i) =>
+      i === index ? { ...t, completed: !t.completed } : t,
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+  <div className="app">
+    <h1>✅ To-Do List</h1>
+    <div className="input-section">
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task..."
+      />
+      <button onClick={addTask}>Add</button>
+    </div>
+
+    <ul>
+      {tasks.map((t, index) => (
+        <li key={index} className={t.completed ? "completed" : ""}>
+          <span onClick={() => toggleTask(index)}>{t.text}</span>
+          <button onClick={() => deleteTask(index)}>❌</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+}
+export default App;
